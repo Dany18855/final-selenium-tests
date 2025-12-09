f407b96a93eee1434f617a232a64c3531f70e021
Dany18855 <clintovich1442806@gmail.com>
2025-12-09 12:12:44 -0800

    Updated Selenium login tests

--- test_login_selenium.py
+++ test_login_selenium.py
@@ -2,44 +2,27 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
 def test_successful_login():
     service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
 
-    try:
-        driver.get("https://letsusedata.com")
+    options = Options()
+    options.add_experimental_option("detach", True) 
 
-        wait = WebDriverWait(driver, 15)
+    driver = webdriver.Chrome(service=service, options=options)
 
-        wait.until(EC.presence_of_element_located((By.TAG_NAME, "body")))
+    driver.get("https://letsusedata.com")
 
-        title = driver.title.lower()
-        url = driver.current_url.lower()
+    wait = WebDriverWait(driver, 10)
 
-        assert "letsusedata" in url or "letsusedata" in title
+    username = wait.until(EC.presence_of_element_located((By.ID, "edit-name")))
+    username.send_keys("username")
 
-    finally:
-        driver.quit()
+    password = driver.find_element(By.ID, "edit-pass")
+    password.send_keys("password")
 
-
-def test_unsuccessful_login():
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
-
-    try:
-        driver.get("https://letsusedata.com")
-
-        wait = WebDriverWait(driver, 15)
-        wait.until(EC.presence_of_element_located((By.TAG_NAME, "body")))
-
-        title = driver.title.lower()
-        url = driver.current_url.lower()
-
-        assert "letsusedata" in url or "letsusedata" in title
-
-    finally:
-        driver.quit()
+    driver.find_element(By.ID, "edit-submit").click()
